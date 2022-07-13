aca09254a1582fbf3c12c6061461c9d02e08f39a
Priyanka-Shahi <shahipriyan@gmail.com>
2022-07-13 21:15:58 +1000

    Arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 
 // Write your code here
+
+let num1 = 2;
+let num2 =31;
+
+let multiply = multiply();
+
+let random = random();
+
+
+let num3 = 19;
+let num4 = 5;
+let mod = mod();
+let max = Math.max(1, 2, 3, 4, 20);
+
+function multiply() {
+    
+    return num1 * num2;
+}
+
+function random() {
+  return Math.floor(Math.random() * 10);
+       
+} 
+
+function mod() {
+ return num3 % num4;
+
+}
+
